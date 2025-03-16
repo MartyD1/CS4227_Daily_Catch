d0825e75162edda92a7bea939697c66a01e10d1c
Marty <coolkidmarty@gmail.com>
2025-03-16 14:45:19 +0000

    Fixed mui compatiability

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -8,20 +8,19 @@ const AppDrawer: React.FC = () => {
 
     return (
         <>
-
             <IconButton onClick={() => setDrawerOpen(true)} style={{ color: "white" }}>
                 <MenuIcon />
             </IconButton>
 
             <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
                 <List style={{ width: "250px" }}>
-                    <ListItem button onClick={() => setDrawerOpen(false)}>
+                    <ListItem onClick={() => setDrawerOpen(false)} sx={{ cursor: "pointer" }}>
                         <ListItemText primary="Close" />
                     </ListItem>
-                    <ListItem button component={Link} to="/profile" onClick={() => setDrawerOpen(false)}>
+                    <ListItem component={Link} to="/profile" onClick={() => setDrawerOpen(false)} sx={{ cursor: "pointer" }}>
                         <ListItemText primary="Profile" />
                     </ListItem>
-                    <ListItem button component={Link} to="/settings" onClick={() => setDrawerOpen(false)}>
+                    <ListItem component={Link} to="/settings" onClick={() => setDrawerOpen(false)} sx={{ cursor: "pointer" }}>
                         <ListItemText primary="Settings" />
                     </ListItem>
                 </List>
